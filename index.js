05cd3e39e8cd1bd3bd73d4633e6fedcb8874965e
rikinurfalakhi <98145185+rikinurfalakhi@users.noreply.github.com>
2023-12-31 22:31:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 4000
 app.get('/', (req, res) => {
     res.send('oke telah berhasil mengakses server')
 })
+app.get('/api', (req, res){
+    res.send('polah');
+})
 app.listen(port, () => {
    console.log(`server berjalan di port ${port}`);
 })
