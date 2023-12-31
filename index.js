9d02513cc81dd4d8f7036d12ca3f79b4113ac2a7
rikinurfalakhi <rikinurfalakhi@gmail.com>
2023-12-31 22:24:09 +0700

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('oke telah berhasil mengakses server')
+})
+app.listen(port, () => {
+   console.log(`server berjalan di port ${port}`);
+})
